d6e11c3bd3181218da387cd64eb7b979d6111de9
septor <patrickweaver@gmail.com>
2010-10-13 12:22:15 -0400

    fixed Digg Service to work with their last redesign

--- lib/Services/Digg.php
+++ lib/Services/Digg.php
@@ -16,7 +16,7 @@
 		public function setVariables( $config ) {
 			$this->username = $config['username'];
 			$this->total = $config['total'];
-			$this->setURLTemplate('http://digg.com/users/'.$config['username'].'/');
+			$this->setURLTemplate('http://digg.com/'.$config['username'].'/');
 		}
 
 		public function buildCache() {
@@ -38,7 +38,7 @@
 		public function __construct( $config ) {
 			parent::setVariables( $config );
 
-			$this->setURL( 'http://digg.com/users/'.$this->username.'/history/diggs.rss' );
+			$this->setURL( 'http://digg.com/'.$this->username.'/diggs.rss' );
 			$this->setItemTemplate('<li><a class="clearfix" href="{{{link}}}">{{{title}}}</a><br /><i>{{{date}}}</i><br />{{{description}}}</li>'."\n");
 
 			parent::__construct( $config );
@@ -72,75 +72,7 @@
 		public function __construct( $config ) {
 			parent::setVariables( $config );
 
-			$this->setURL( 'http://digg.com/users/'.$this->username.'/history/submissions.rss' );
-			$this->setItemTemplate('<li><a class="clearfix" href="{{{link}}}">{{{title}}}</a><br /><i>{{{date}}}</i><br />{{{description}}}</li>'."\n");
-
-			parent::__construct( $config );
-		}
-
-		/**
-		 * @return SimpleXMLElement
-		 */
-		public function getData() {
-			$data = parent::getData();
-			return $data->channel->item;
-		}
-
-		/**
-		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			return array(
-						'link' => htmlspecialchars( $item->link ),
-						'title' => $item->title,
-						'description' => $item->description,
-						'date' => Pubwich::time_since( $item->pubDate ),
-						'author' => $item->author,
-						);
-		}
-
-	}
-
-	class History extends Digg {
-
-		public function __construct( $config ) {
-			parent::setVariables( $config );
-
-			$this->setURL( 'http://digg.com/users/'.$this->username.'/history/history.rss' );
-			$this->setItemTemplate('<li><a class="clearfix" href="{{{link}}}">{{{title}}}</a><br /><i>{{{date}}}</i><br />{{{description}}}</li>'."\n");
-
-			parent::__construct( $config );
-		}
-
-		/**
-		 * @return SimpleXMLElement
-		 */
-		public function getData() {
-			$data = parent::getData();
-			return $data->channel->item;
-		}
-
-		/**
-		 * @return array
-		 */
-		public function populateItemTemplate( &$item ) {
-			return array(
-						'link' => htmlspecialchars( $item->link ),
-						'title' => $item->title,
-						'description' => $item->description,
-						'date' => Pubwich::time_since( $item->pubDate ),
-						'author' => $item->author,
-						);
-		}
-
-	}
-
-	class Favorites extends Digg {
-
-		public function __construct( $config ) {
-			parent::setVariables( $config );
-
-			$this->setURL( 'http://digg.com/users/'.$this->username.'/history/favorites.rss' );
+			$this->setURL( 'http://digg.com/'.$this->username.'/submissions.rss' );
 			$this->setItemTemplate('<li><a class="clearfix" href="{{{link}}}">{{{title}}}</a><br /><i>{{{date}}}</i><br />{{{description}}}</li>'."\n");
 
 			parent::__construct( $config );
